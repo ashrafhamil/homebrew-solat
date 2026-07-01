b86ee5e91f2ca8bb0e4007f745bae7878029f4fc
ashrafhamil <ashrafhamily@gmail.com>
2026-07-01 11:39:24 +0800

    Remove solat-watch daemon, keep solat CLI only

--- Formula/solat.rb
+++ Formula/solat.rb
@@ -1,22 +1,14 @@
 class Solat < Formula
-  desc "Malaysia-aware prayer times CLI with azan notification"
+  desc "Malaysia-aware prayer times CLI"
   homepage "https://github.com/ashrafhamil/homebrew-solat"
   url "https://github.com/ashrafhamil/homebrew-solat.git", branch: "main"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   depends_on "jq"
 
   def install
     bin.install "solat"
-    bin.install "solat-watch"
-  end
-
-  service do
-    run [opt_bin/"solat-watch"]
-    keep_alive true
-    log_path var/"log/solat-watch.log"
-    error_log_path var/"log/solat-watch.err.log"
   end
 
   test do
